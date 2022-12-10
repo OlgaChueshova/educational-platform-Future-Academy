fa3c2dc2294782dbfcd7fb62d262cb0a97a6818d
Chueshova Olga <chueshova_olga@mail.ru>
2022-12-10 23:06:22 +0300

    add gradient

--- src/components/atoms/Link/Link.js
+++ src/components/atoms/Link/Link.js
@@ -4,22 +4,63 @@ import './link.scss';
 export class Link extends Component {
     constructor() {
         super();
-        this.props = JSON.parse(this.getAttribute('link'))
+        this.props = JSON.parse(this.getAttribute('link'));
+        this.state = {
+            isActive: false
+        }
+        this.onMousemove = this.onMousemove.bind(this);
+        this.onMouseout = this.onMouseout.bind(this);
+    }
+
+    onMousemove() {
+        this.setState((state) => {
+            return {
+                ...state,
+                isActive: true,
+            }
+        });
+        this.render();
+    }
+
+    onMouseout() {
+        this.setState((state) => {
+            return {
+                ...state,
+                isActive: false,
+            }
+        });
+        this.render();
+    }
+
+    componentDidMount() {
+        this.addEventListener('mousemove', this.onMousemove);
+        this.addEventListener('mouseout', this.onMouseout);
+    }
+
+    componentWillUnmount() {
+        this.removeEventListener('mousemove', this.onMousemove);
+        this.removeEventListener('mouseout', this.onMouseout);
     }
 
     static get observedAttributes() {
         return ['link']
     }
-
+ 
     render() {
         return `
             <a href='${this.props.path}'>
-                ${this.props.icon 
-                    ? `<img src='${this.props.icon}' alt="hamburger" class="header__navigation--icon">`
-                    : ''}
+                ${this.props.icon
+                ? `<img src="${this.state.isActive 
+                        ? `${this.props.icon2 
+                                ? `${this.props.icon2}`
+                                : `${this.props.icon}`
+                            }` 
+                        : `${this.props.icon}`}" 
+                    alt="${this.props.label}">`
+                : ''}
                 <span>${this.props.title ? `${this.props.title}` : ''}</span>
             </a>
-        `
+            `
     }
 }
 

258523d4b0a0288ada5237f52757a7a1da1eccb5
nts30 <alex0611f@gmail.com>
2022-12-23 19:40:51 +0300

    changed api key

--- src/services/Cloud.js
+++ src/services/Cloud.js
@@ -4,12 +4,12 @@ import {API_KEY, APP_ID} from '../constants/envValues'
 export class CloudService {
     constructor() {
         this.Config = {
-            apiKey: API_KEY,
+            apiKey: "AIzaSyBjKxISWq9yonbb26YmYIiGNnj9330nKW8",
             authDomain: "future-academy-36385.firebaseapp.com",
             projectId: "future-academy-36385",
             storageBucket: "future-academy-36385.appspot.com",
             messagingSenderId: "432812923360",
-            appId: APP_ID,
+            appId: "1:432812923360:web:3e083d71caa945925a3385",
             measurementId: "G-GTTQNMELVE"
         };
 
